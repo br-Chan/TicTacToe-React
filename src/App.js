54d79efc7634c2541927ead4d229a25aabed51b0
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-03-21 10:59:59 +1300

    Add new top level component Game, which returns board and info of game

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const[squares, setSquares] = useState(Array(9).fill(null));
 
@@ -22,8 +22,7 @@ export default function Board() {
     else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   // Status section
@@ -57,6 +56,27 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const[history, setHistory] = useState([Array(9).fill(null)]); // array of single item, which is array of 9 nulls
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>/*TODO*/</ol>
+    </div>
+    </div>
   );
 }
 
